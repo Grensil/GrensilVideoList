1fa243efe6329021ec5637c07ad1d100ad420a7f
grensil <jungdj1225@gmail.com>
2025-11-03 20:13:17 +0900

    develop: videoitem, photoitem component

--- feature/main/src/main/java/com/example/main/component/PhotoItem.kt
+++ feature/main/src/main/java/com/example/main/component/PhotoItem.kt
@@ -1,15 +1,30 @@
 package com.example.main.component
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import coil.compose.AsyncImage
+import com.example.designsystem.theme.PurpleGrey40
+import com.example.designsystem.theme.PurpleGrey80
 import com.example.domain.model.Photo
+import com.example.main.R
 
 
 @Composable
@@ -17,32 +32,43 @@ fun PhotoItem(photo: Photo) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .height(120.dp)
             .padding(horizontal = 16.dp)
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(
-                text = "PHOTO",
-                style = MaterialTheme.typography.labelSmall,
-                color = MaterialTheme.colorScheme.secondary
+
+        Box(contentAlignment = Alignment.TopCenter) {
+
+            Image(
+                painter = painterResource(id = R.drawable.icon_photo),
+                contentDescription = "photo icon",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .size(48.dp)
+                    .zIndex(1f)
+                    .background(PurpleGrey40)
+                    .align(Alignment.TopStart),
+                alignment = Alignment.TopStart
             )
-            Text(
-                text = "ID: ${photo.id}",
-                style = MaterialTheme.typography.titleMedium
+
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(120.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(PurpleGrey80),
+                model = photo.src.original,
+                contentScale = ContentScale.Crop,
+                contentDescription = "video  image"
             )
-            Text(
-                text = "Photographer: ${photo.photographer}",
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Text(
-                text = "Size: ${photo.width}x${photo.height}",
-                style = MaterialTheme.typography.bodySmall
-            )
-            Text(
-                text = "Alt: ${photo.alt}",
-                style = MaterialTheme.typography.bodySmall
-            )
+
+            Row(modifier = Modifier.fillMaxSize(), verticalAlignment = Alignment.Bottom) {
+                Text(
+                    text = "Photographer: ${photo.photographer}",
+                    color = PurpleGrey80
+                )
+            }
         }
     }
 }
--- feature/main/src/main/java/com/example/main/component/VideoItem.kt
+++ feature/main/src/main/java/com/example/main/component/VideoItem.kt
@@ -1,92 +1,127 @@
 package com.example.main.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
+import com.example.designsystem.theme.GrensilVideoListTheme
+import com.example.designsystem.theme.PurpleGrey40
 import com.example.designsystem.theme.PurpleGrey80
 import com.example.domain.model.Video
 import com.example.domain.model.VideoFile
+import com.example.domain.model.VideoPicture
 import com.example.domain.model.VideoUser
+import com.example.main.R
 
 
 @Composable
 fun VideoItem(video: Video) {
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .height(120.dp)
             .padding(horizontal = 16.dp)
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(
-                text = "VIDEO",
-                style = MaterialTheme.typography.labelSmall,
-                color = MaterialTheme.colorScheme.primary
-            )
+        Box(contentAlignment = Alignment.TopCenter) {
 
-            Text(
-                text = "User: ${video.user.name}",
-                style = MaterialTheme.typography.bodyMedium
+            Image(
+                painter = painterResource(id = R.drawable.icon_video),
+                contentDescription = "photo icon",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .size(48.dp)
+                    .zIndex(1f)
+                    .background(PurpleGrey40)
+                    .align(Alignment.TopStart),
+                alignment = Alignment.TopStart
             )
 
             AsyncImage(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .size(120.dp)
                     .clip(RoundedCornerShape(16.dp))
                     .background(PurpleGrey80),
                 model = video.videoPictures?.get(0)?.picture,
                 contentScale = ContentScale.Crop,
-                contentDescription = "article image"
+                contentDescription = "video  image"
             )
 
-            Text(
-                text = "Duration: ${video}s",
-                style = MaterialTheme.typography.bodySmall
-            )
+            Row(modifier = Modifier.fillMaxSize(), verticalAlignment = Alignment.Bottom) {
+
+                Text(
+                    text = video.user.name,
+                    color = PurpleGrey80
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(
+                    text = "Duration: ${video.duration}s",
+                    color = PurpleGrey80
+                )
+            }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PreviewVideoItem() {
-    VideoItem(
-        Video(
-            id = 1,
-            width = 400,
-            height = 200,
-            url = "url",
-            image = "image",
-            duration = 55,
-            user = VideoUser(
-                id = 222,
-                name = "name",
-                url = "url"
-            ),
-            videoFiles = listOf(
-                VideoFile(
-                    id = 344,
-                    quality = "HD",
-                    fileType = "fileType",
-                    width = 400,
-                    height = 200,
-                    link = "link"
+    GrensilVideoListTheme {
+        VideoItem(
+            Video(
+                id = 1,
+                width = 400,
+                height = 200,
+                url = "url",
+                image = "image",
+                duration = 55,
+                user = VideoUser(
+                    id = 222,
+                    name = "name",
+                    url = "url"
+                ),
+                videoFiles = listOf(
+                    VideoFile(
+                        id = 344,
+                        quality = "HD",
+                        fileType = "fileType",
+                        width = 400,
+                        height = 200,
+                        link = "link"
+                    )
+                ),
+                videoPictures = listOf(
+                    VideoPicture(
+                        id = 1,
+                        picture = "https://images.pexels.com/videos/3571264/free-video-3571264.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+                        nr = 0
+                    )
                 )
-            ),
+            )
         )
-    )
+    }
 }
